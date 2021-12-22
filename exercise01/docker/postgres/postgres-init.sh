38632647285387ecc1d0c277a6aa62e17099ed3d
chiosam <monica.chiosa@inf.ethz.ch>
2021-12-22 18:08:50 +0100

    [Exam] Jupyter notebooks updated for exam practice
    PostgreSQL - cleaner music dataset (discogs)    - use exercise01 docker-compose
    PySpark    - new dataset (movies)               - use exercise08 docker-compose
    SparkSQL   - same dataset as exercises (orders) - use exercise09 docker-compose
    RumbleDB   - new dataset (github)               - use exercise11 docker-compose/ VM/ local installation

--- exercise01/docker/postgres/postgres-init.sh
+++ exercise01/docker/postgres/postgres-init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 createdb -U postgres "discogs"
-curl https://cloud.inf.ethz.ch/s/4bZWo4TjeXgCNz5/download/discogs.dump.xz | xz -d | \
+curl https://cloud.inf.ethz.ch/s/X6g4ScWRX2wBC4t/download/discogsexam.dump.xz | xz -d | \
     psql -U postgres --set ON_ERROR_STOP=on "discogs"
+
+

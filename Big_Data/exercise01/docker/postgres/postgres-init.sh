7d176255abb8f3866319c4093a39d2be9aace32e
Jingyi Zhu <jinzhu@student.ethz.ch>
2022-09-22 21:26:18 +0200

    update discogs link and postgres Dockerfile

--- Big_Data/exercise01/docker/postgres/postgres-init.sh
+++ Big_Data/exercise01/docker/postgres/postgres-init.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 createdb -U postgres "discogs"
-curl https://cloud.inf.ethz.ch/s/X6g4ScWRX2wBC4t/download/discogsexam.dump.xz | xz -d | \
+curl https://cloud.inf.ethz.ch/s/DtjCHTLRHT39BRN/download/discogs.dump.xz | xz -d | \
     psql -U postgres --set ON_ERROR_STOP=on "discogs"
-
-

6caac0e0715e8a789cb44e202dc4a25d42527680
WenqiJiang <wenqi.j@columbia.edu>
2023-02-21 13:27:02 +0100

    update exercise 00

--- Big_Data_For_Engineers/exercise00/docker/postgres/postgres-init.sh
+++ Big_Data_For_Engineers/exercise00/docker/postgres/postgres-init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 createdb -U postgres "beer.stackexchange.com"
-curl -sS https://cloud.inf.ethz.ch/s/ik3FjCA67p33gZD/download/beer.stackexchange.com.dump.xz | xz -d | \
+curl -sS https://cloud.inf.ethz.ch/s/LkqPXez58NBtPmy/download/beer.stackexchange.com.dump.xz | xz -d | \
     psql -U postgres --set ON_ERROR_STOP=on "beer.stackexchange.com"

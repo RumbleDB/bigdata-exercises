8e77f1ac7a37385575d9a276017e7ef21eadfd84
zhe <zhe@inf.ethz.ch>
2024-02-19 13:09:25 +0100

    Update ex00

--- Big_Data_For_Engineers/exercise00/docker/postgres/postgres-init.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-createdb -U postgres "beer.stackexchange.com"
-curl -sS https://cloud.inf.ethz.ch/s/LkqPXez58NBtPmy/download/beer.stackexchange.com.dump.xz | xz -d | \
-    psql -U postgres --set ON_ERROR_STOP=on "beer.stackexchange.com"
